ab7297e56cd31af32b2d471f3283f95a5e51b8ea
Preacher-Y <ysheja@gmail.com>
2026-02-08 10:58:25 +0200

    Starting Question 5

---
+++ question5-task_mangement-api/src/main/java/com/example/question5_task_mangement_api/Question5TaskMangementApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.question5_task_mangement_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Question5TaskMangementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Question5TaskMangementApiApplication.class, args);
+	}
+
+}
---
+++ question5-task_mangement-api/src/test/java/com/example/question5_task_mangement_api/Question5TaskMangementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.question5_task_mangement_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Question5TaskMangementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
