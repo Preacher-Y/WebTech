3a0316f5b75fb3e427e11ab5b8c8cfa8c63b112a
Preacher-Y <ysheja@gmail.com>
2026-02-08 16:09:32 +0200

    Starting the Bonus Question

---
+++ questionbonus-userprofile-api/src/main/java/com/example/questionbonus_userprofile_api/QuestionbonusUserprofileApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.questionbonus_userprofile_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionbonusUserprofileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionbonusUserprofileApiApplication.class, args);
+	}
+
+}
---
+++ questionbonus-userprofile-api/src/test/java/com/example/questionbonus_userprofile_api/QuestionbonusUserprofileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questionbonus_userprofile_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionbonusUserprofileApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
